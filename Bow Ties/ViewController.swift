256b6abd9ff2b9f88e0763b2dbefa3e9ff05c443
Amitabh <amitabhis2903@gmail.com>
2019-05-23 23:44:44 +0530

    Initial Commit

---
+++ Bow Ties/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bow Ties
+//
+//  Created by A on 23/05/19.
+//  Copyright © 2019 A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
